00041218ae0416c1d345d826128ef9841441f710
yufengyang <yufengyang@xianlai-inc.com>
2023-11-30 10:21:49 +0800

    Obtain the idfv

--- ios/Classes/FlutterDeviceIdPlugin.swift
+++ ios/Classes/FlutterDeviceIdPlugin.swift
@@ -2,18 +2,22 @@ import Flutter
 import UIKit
 
 public class FlutterDeviceIdPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_device_id", binaryMessenger: registrar.messenger())
-    let instance = FlutterDeviceIdPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: FlutterDeviceIdPluginConstants.METHOD_CHANNEL_NAME, binaryMessenger: registrar.messenger())
+        let instance = FlutterDeviceIdPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case FlutterDeviceIdPluginConstants.DEVICE_ID:
+            result(getIDFV())
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+    
+    private func getIDFV() -> String? {
+        return UIDevice.current.identifierForVendor?.uuidString
+    }
 }
---
+++ ios/Classes/FlutterDeviceIdPluginConstants.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+@objcMembers
+public class FlutterDeviceIdPluginConstants: NSObject {
+    static var METHOD_CHANNEL_NAME    = "plugins.flutter.io/flutter_device_id"
+    static var DEVICE_ID              = "device_id"
+}
